a403c8c3e2f25680ca39ad27fd47ef1d064b23b4
Kodongwoo <ehddn2202@naver.com>
2023-02-03 19:45:39 +0900

    feat(003-EMAIL-VERIFICATION): 커스터머(고객)에 대한 email 인증 서비스 구현

--- user-api/src/main/java/com/zerobase/cms/user/client/domain/repository/CustomRepository.java
+++ user-api/src/main/java/com/zerobase/cms/user/client/domain/repository/CustomRepository.java
@@ -1,8 +1,11 @@
 package com.zerobase.cms.user.client.domain.repository;
 
 import com.zerobase.cms.user.client.domain.model.Customer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
 
 }
--- user-api/src/main/java/com/zerobase/cms/user/client/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/zerobase/cms/user/client/service/SignUpCustomerService.java
@@ -3,6 +3,12 @@ package com.zerobase.cms.user.client.service;
 import com.zerobase.cms.user.client.domain.SignUpform;
 import com.zerobase.cms.user.client.domain.model.Customer;
 import com.zerobase.cms.user.client.domain.repository.CustomRepository;
+import com.zerobase.cms.user.client.exception.CustomException;
+import com.zerobase.cms.user.client.exception.ErrorCode;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +21,40 @@ public class SignUpCustomerService {
     public Customer signUp(SignUpform form) {
         return customRepository.save(Customer.from(form));
     }
+
+    public boolean isEmailExist(String email) {
+        return customRepository.findByEmail(email.toLowerCase(Locale.ROOT))
+            .isPresent();
+    }
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer = customRepository.findByEmail(email)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+        if (customer.isVerify()) {
+            throw new CustomException(ErrorCode.ALREADY_VERIFY);
+        } else if (!customer.getVerificationCode().equals(code)) {
+            throw new CustomException(ErrorCode.WRONG_VERIFICATION);
+        } else if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(ErrorCode.EXPIRE_CODE);
+        }
+        customer.setVerify(true);
+
+    }
+
+    @Transactional
+    public LocalDateTime ChangeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> customerOptional = customRepository.findById(customerId);
+
+        if (customerOptional.isEmpty()) {
+            //예외처리
+            throw new CustomException(ErrorCode.NOT_FOUND_USER);
+
+        }
+        Customer customer = customerOptional.get();
+        customer.setVerificationCode(verificationCode);
+        customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1));
+        return customer.getVerifyExpiredAt();
+    }
+
 
 }
